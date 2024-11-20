96e6832b5f1faa9746473ba4a0f4959d3bd4f64c
KajavathananThevapalan <ntkayavathanan2699@gmail.com>
2024-11-20 11:01:39 +0530

    Reservation update

--- CarRendalAPI/obj/Debug/net8.0/CarRendalAPI.AssemblyInfo.cs
+++ CarRendalAPI/obj/Debug/net8.0/CarRendalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRendalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe56826a91293b9ab3dcf527a1fdb3f2b1cf75a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0f995edd2b597bdaa6dfb48ea6ebe701d0c385")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRendalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRendalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
